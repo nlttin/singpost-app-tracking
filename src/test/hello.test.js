7a963ab64c48677a64836fd92026aaad728bd6b1
v-nicknguyen-be <v-nick.nguyen@singpost.com>
2026-04-24 21:19:25 +0700

    Update hello.test.js

--- src/test/hello.test.js
+++ src/test/hello.test.js
@@ -1,7 +1,7 @@
 const test = require("node:test");
 const assert = require("node:assert");
 //const { hello } = require("../src/hello");
-const hello = require('../hello'); 
+const { hello } = require('../hello'); 
 test("hello returns expected message", () => {
   assert.strictEqual(hello("AppTracking"), "Hello, AppTracking!");
 });
